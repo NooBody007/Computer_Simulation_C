2df9f36f6431c6c2cdd51b8e2a9f213c11eb2cf4
NooBody007 <e.f.player-1.a@outlook.com>
2023-03-10 13:08:57 +0300

    Header file created, notepad and time struct added.

---
+++ comp_sim.h
@@ -0,0 +1,30 @@
+
+#infdef __COMP_SIM_H__
+#define __COMP_SIM_H__
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+
+struct textfile {
+	int id, hours, minutes, day, month, year;
+	char file_name[20];
+	time_t now;
+}notepad;
+
+struct tm *local;
+
+
+
+
+
+
+
+
+#endif
