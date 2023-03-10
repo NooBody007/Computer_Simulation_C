4deb2caf3d8d5ac96754ae5a7dd11bc811b69ff2
NooBody007 <e.f.player-1.a@outlook.com>
2023-03-10 20:57:57 +0300

    Header file, notepad function declarations.

--- comp_sim.h
+++ comp_sim.h
@@ -20,11 +20,20 @@ struct textfile {
 
 struct tm *local;
 
+//creates a notepad file
+int create_notepad(int id,char file_name[30],int hours, int minutes, int day, int month, int year);
 
+//writes to selected notepad file
+int write_notepad(int id);
 
+//reads the selected notepad file
+int read_notepad(int id);
 
+//deletes the selected notepad file
+int delete_notepad(int id);
 
-
+//shows all of the notepad files
+int view_notepads(void);
 
 
 #endif
