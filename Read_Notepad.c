1b13024967657614a710fa43d9b8fbd8c4ed77e9
NooBody007 <e.f.player-1.a@outlook.com>
2023-03-28 01:24:26 +0300

    Read_Notepad, whole file created.

---
+++ Read_Notepad.c
@@ -0,0 +1,26 @@
+#include "comp_sim.h"
+
+
+int read_notepad(int id)
+{
+	FILE *fp = NULL;
+	
+	fp = fopen("notepads.dat","r");
+	
+	if(fp == NULL){
+		printf("An error occured while opening the file.\n");
+		return 1;
+	}
+	
+	else{
+		notepad *w_notepad = (notepad*)malloc(sizeof(notepad));
+		
+		while(fread(w_notepad,sizeof(notepad),1,fp)){
+			printf("\n%d\t\t\t%s\t\t\t",w_notepad->np_id,w_notepad->np_file_name);
+		}
+		close(fp);
+		free(w_notepad);
+		return 0;
+	}
+	
+}
